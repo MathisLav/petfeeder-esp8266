8cde05a9704155caa5532845e74a789b7f0d0c5c
pedroernesto00 <pedroernesto2406@alu.ufc.br>
2022-01-19 14:00:20 +0100

    Add available wifi networks list

--- src/processors.cpp
+++ src/processors.cpp
@@ -6,11 +6,20 @@
 #include <ESP8266WiFi.h>
  
 String indexStateTemplate(const String &value) {
+    int n = WiFi.scanComplete();
     if(value == "STATE") {
         if(WiFi.status() == WL_CONNECTED)
             return "Connected! You can use the petfeeder.";
         else
             return "Offline. Please provide valid WiFi credentials.";
+    } else if (value == "SSIDS") {
+        String networks = "";
+        for (int i = 0; i < n - 1; i++) {
+            networks += WiFi.SSID(i) + ",";
+        }
+        networks += WiFi.SSID(n-1);
+        return networks;
     }
+
     return "";
 }
